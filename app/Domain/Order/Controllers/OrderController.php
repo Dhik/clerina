57d307f1256398b4fd1655eb732b8ca5a2e0dab5
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-07-25 11:36:46 +0700

    fetch data

--- app/Domain/Order/Controllers/OrderController.php
+++ app/Domain/Order/Controllers/OrderController.php
@@ -277,7 +277,7 @@ class OrderController extends Controller
         'x-api-key' => 'f5c80067e1da48e0b2b124558f5c533f1fda9fea72aa4a2a866c6a15a1a31ca8'
     ];
     $statuses = ['paid', 'process', 'pick', 'packing', 'packed', 'sent', 'completed'];
-    $startDate = '2024-07-22';
+    $startDate = '2024-07-01';
     $endDate = '2024-07-23';
     $allOrders = [];
 
