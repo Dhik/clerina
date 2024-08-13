367e2d103f3ae53f002e223ee8ebf633ffbe07f0
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-08-13 10:28:46 +0700

    update orders

--- app/Domain/Order/Controllers/OrderController.php
+++ app/Domain/Order/Controllers/OrderController.php
@@ -278,8 +278,8 @@ class OrderController extends Controller
         'x-api-key' => 'f5c80067e1da48e0b2b124558f5c533f1fda9fea72aa4a2a866c6a15a1a31ca8'
     ];
     $statuses = ['paid', 'process', 'pick', 'packing', 'packed', 'sent', 'completed'];
-    $startDate = '2024-07-08';
-    $endDate = '2024-07-12';
+    $startDate = '2024-07-12';
+    $endDate = '2024-07-16';
     $allOrders = [];
 
     foreach ($statuses as $status) {
