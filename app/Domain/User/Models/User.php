4d505ddb319b9be9ac63924d038dac23dfe5d19c
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-09-17 16:11:31 +0700

    update

--- app/Domain/User/Models/User.php
+++ app/Domain/User/Models/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
 
         self::addGlobalScope(function (Builder $builder) {
             if (Auth::hasUser()) {
-                if (! Auth::user()->hasRole(RoleEnum::SuperAdmin)) {
+                if (! Auth::user()->hasAnyRole([RoleEnum::SuperAdmin, RoleEnum::HR])) {
                     $builder->whereHas('tenants', function ($q) {
                         $q->where('id', Auth::user()->current_tenant_id);
                     });
