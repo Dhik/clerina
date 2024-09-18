92327a3585de8dd8ffbef8bdf4e1ad386b71fe09
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-09-18 16:20:45 +0700

    update

--- resources/views/admin/employee/new_app.blade.php
+++ resources/views/admin/employee/new_app.blade.php
@@ -231,7 +231,7 @@
                     document.getElementById('distanceToTarget').textContent = `You are ${distance.toFixed(2)} km away from your workplace.`;
 
                     // Enable the button if within 0.2 km radius
-                    if (distance <= 20) {
+                    if (distance <= 200) {
                         document.getElementById("clockInBtn").disabled = false;
                         hideLocationError(); 
                     } else {
