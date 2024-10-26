5c03e0dcebe3049b251257dbadc119f644ccb553
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-10-26 10:15:12 +0700

    update

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -9,6 +9,15 @@
 
     {{-- Sidebar menu --}}
     <div class="sidebar">
+        <div class="px-3 py-2">
+            <a href="https://huggingface.co/spaces/dhikri/clerina_data_analyzer" target="_blank">
+                <button type="button" class="btn btn-block" 
+                        style="background-color: #0A1D37; color: white; border-color: #071C2D; padding: 10px;">
+                    Analysis Report
+                </button>
+            </a>
+        </div>
+
         <nav class="pt-2">
             <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}"
                 data-widget="treeview" role="menu"
