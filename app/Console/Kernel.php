ed7ada2f1c209e9b7341d3cc2e41487dd9db11d6
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-10-31 17:23:12 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,14 +21,14 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('sales:create')->dailyAt('00:01')->withoutOverlapping();
-        // $schedule->command('marketing:create')->dailyAt('00:02')->withoutOverlapping();
-        // $schedule->command('data:scrap')->dailyAt('04:00');
-        // $schedule->command('data:scrap-contest')->dailyAt('05:00');
-        // $schedule->command('statistic:campaign-recap')->dailyAt('07:00');
-        // $schedule->command('orders:fetch-external')->cron('0 9,12,17,19,21,3,6 * * *')->timezone('Asia/Jakarta');
-        // $schedule->command('attendance:populate')->dailyAt('00:05');
-        // $schedule->command('campaign:refresh-contents')->dailyAt('06:00');
+        $schedule->command('sales:create')->dailyAt('00:01')->withoutOverlapping();
+        $schedule->command('marketing:create')->dailyAt('00:02')->withoutOverlapping();
+        $schedule->command('data:scrap')->dailyAt('04:00');
+        $schedule->command('data:scrap-contest')->dailyAt('05:00');
+        $schedule->command('statistic:campaign-recap')->dailyAt('07:00');
+        $schedule->command('orders:fetch-external')->cron('0 9,12,17,19,21,3,6 * * *')->timezone('Asia/Jakarta');
+        $schedule->command('attendance:populate')->dailyAt('00:05');
+        $schedule->command('campaign:refresh-contents')->dailyAt('06:00');
         // $schedule->command('report:send-telegram')->dailyAt('07:00');
         $schedule->command('report:send-telegram')->dailyAt('17:12');
 
