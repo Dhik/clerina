b09ae381e39bb0881eb4813d048362b037b9ca54
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-11-06 15:23:07 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,12 +26,12 @@ class Kernel extends ConsoleKernel
         $schedule->command('marketing:create')->dailyAt('00:02')->withoutOverlapping();
         $schedule->command('data:scrap')->dailyAt('04:00');
         $schedule->command('data:scrap-contest')->dailyAt('05:00');
-        $schedule->command('statistic:campaign-recap')->dailyAt('07:00');
+        $schedule->command('statistic:campaign-recap')->dailyAt('05:30');
         $schedule->command('orders:fetch-external')->cron('0 9,12,17,19,21,3,6 * * *')->timezone('Asia/Jakarta');
         $schedule->command('attendance:populate')->dailyAt('00:05');
-        $schedule->command('campaign:refresh-contents')->dailyAt('06:00');
+        $schedule->command('campaign:refresh-contents')->dailyAt('03:00');
         $schedule->command('report:send-telegram')->dailyAt('07:00');
-        $schedule->command('google-sheet:import')->dailyAt('15:15');
+        $schedule->command('google-sheet:import')->dailyAt('06:00');
 
 
 
