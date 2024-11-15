caaed7dabbc30e2f675d783428ed9a9c11f049c9
Dhik <mohammad18018@mail.unpad.ac.id>
2024-11-15 13:17:01 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('orders:fetch-external')->cron('0 9,12,17,19,21,3,6 * * *')->timezone('Asia/Jakarta');
         $schedule->command('attendance:populate')->dailyAt('00:05');
         $schedule->command('campaign:refresh-contents')->dailyAt('03:00');
-        $schedule->command('report:send-telegram')->dailyAt('07:00');
+        $schedule->command('report:send-telegram')->dailyAt('15:30');
         $schedule->command('google-sheet:import')->dailyAt('06:00');
         $schedule->command('import:visit')->dailyAt('06:00');
 
