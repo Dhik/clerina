4add3581a160440f936ff81432b1240d5d3f96de
Dhik <mohammad18018@mail.unpad.ac.id>
2024-11-26 13:00:53 +0700

    update

--- app/Domain/Talent/Controllers/TalentController.php
+++ app/Domain/Talent/Controllers/TalentController.php
@@ -220,7 +220,7 @@ class TalentController extends Controller
             $approval = Approval::findOrFail($approvalId);
         }
 
-        $harga = $talent->rate_per_slot * $talent->slot_final; 
+        $harga = $talent->rate_final;  
         $slot_final = $talent->slot_final;
         if (!is_null($talent->tax_percentage) && $talent->tax_percentage > 0) {
             $pphPercentage = $talent->tax_percentage;
