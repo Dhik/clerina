755972a4b537a715d497359285e16d9bd38f0642
Dhik <mohammad18018@mail.unpad.ac.id>
2025-01-06 15:42:50 +0700

    update

--- app/Domain/Order/Controllers/OrderController.php
+++ app/Domain/Order/Controllers/OrderController.php
@@ -553,8 +553,8 @@ class OrderController extends Controller
         // $startDate = Carbon::now()->startOfMonth()->format('Y-m-d');
         // $endDate = Carbon::now()->format('Y-m-d');
 
-        $startDate = '2024-12-24';
-        $endDate = '2024-12-26';
+        $startDate = '2024-12-26';
+        $endDate = '2024-12-28';
 
         try {
             $page = 1;
