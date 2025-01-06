292ae2716393fcfad4f2a4ff86910e05151ab5e0
Dhik <mohammad18018@mail.unpad.ac.id>
2025-01-06 11:52:47 +0700

    update

--- app/Domain/Order/Controllers/OrderController.php
+++ app/Domain/Order/Controllers/OrderController.php
@@ -487,8 +487,8 @@ class OrderController extends Controller
             'x-api-key' => 'f5c80067e1da48e0b2b124558f5c533f1fda9fea72aa4a2a866c6a15a1a31ca8'
         ];
         
-        $startDate = Carbon::now()->startOfMonth()->format('Y-m-d');
-        $endDate = Carbon::now()->format('Y-m-d');
+        // $startDate = Carbon::now()->startOfMonth()->format('Y-m-d');
+        // $endDate = Carbon::now()->format('Y-m-d');
 
         $startDate = '2024-11-30';
         $endDate = '2024-12-02';
