6db776a5f3461986a05774c06c9dafe92f8909eb
Dhik <mohammad18018@mail.unpad.ac.id>
2025-01-23 10:57:17 +0700

    update

--- resources/views/admin/customers_analysis/index.blade.php
+++ resources/views/admin/customers_analysis/index.blade.php
@@ -20,12 +20,14 @@
                                 <option value="">All Produk</option>
                             </select>
                         </div>
-                        <select id="filterStatus" class="form-control select2">
-                            <option value="">All Status</option>
-                            @foreach($customer as $status)
-                                <option value="{{ $status->status_customer }}">{{ $status->status_customer }}</option>
-                            @endforeach
-                        </select>
+                        <div class="col-3">
+                            <select id="filterStatus" class="form-control select2">
+                                <option value="">All Status</option>
+                                @foreach($customer as $status)
+                                    <option value="{{ $status->status_customer }}">{{ $status->status_customer }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                         <div class="col-auto">
                             <div class="btn-group">
                             <button id="exportButton" class="btn btn-success"><i class="fas fa-file-excel"></i> Export to Excel</button>
