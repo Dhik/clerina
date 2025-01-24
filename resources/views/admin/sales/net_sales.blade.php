fbd483edc53bf0cb56cf1862ddc53b5d07d6790f
Dhik <mohammad18018@mail.unpad.ac.id>
2025-01-24 13:39:31 +0700

    update

--- resources/views/admin/sales/net_sales.blade.php
+++ resources/views/admin/sales/net_sales.blade.php
@@ -217,7 +217,7 @@
 </div>
 
 <div class="modal fade" id="kolDetailModal">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">KOL Detail</h5>
