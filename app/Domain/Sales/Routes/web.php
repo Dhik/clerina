b658451ee947498bd1cc17d020fe9d38a54c4a09
Dhik <mohammad18018@mail.unpad.ac.id>
2025-01-25 12:07:40 +0700

    update

--- app/Domain/Sales/Routes/web.php
+++ app/Domain/Sales/Routes/web.php
@@ -69,8 +69,8 @@ Route::prefix('admin')
 
         Route::prefix('net-profit')->group(function () {
             Route::get('/update-spent-kol', [NetProfitController::class, 'updateSpentKol'])->name('net-profit.update-spent-kol');
-            Route::get('/update-hpp', [NetProfitController::class, 'updateHpp'])->name('net-profit.update-hpp');
             Route::get('/import-data', [NetProfitController::class, 'importNetProfits'])->name('net-profit.import-data');
+            Route::get('/update-hpp', [NetProfitController::class, 'updateHpp'])->name('net-profit.update-hpp');
             Route::get('/hpp-by-date', [NetProfitController::class, 'getHppByDate'])->name('net-profit.getHppByDate');
         });
 
