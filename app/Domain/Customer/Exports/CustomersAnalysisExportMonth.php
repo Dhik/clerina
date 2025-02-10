26e3264473d51b5584e5fb62e328cb66a3d31e99
Dhik <mohammad18018@mail.unpad.ac.id>
2025-02-10 14:39:52 +0700

    update

--- app/Domain/Customer/Exports/CustomersAnalysisExportMonth.php
+++ app/Domain/Customer/Exports/CustomersAnalysisExportMonth.php
@@ -11,7 +11,7 @@ class CustomersAnalysisExportMonth implements FromCollection, WithHeadings
     public function collection() 
     {
         return CustomersAnalysis::query()
-            ->whereBetween('tanggal_pesanan_dibuat', ['2025-01-01', '2025-01-05'])
+            ->whereBetween('tanggal_pesanan_dibuat', ['2025-01-06', '2025-01-10'])
             ->select([
                 'nama_penerima',
                 'nomor_telepon',
