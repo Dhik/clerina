3e6bc4464c27c39736ea0caba5efbe51563508db
Dhik <mohammad18018@mail.unpad.ac.id>
2025-03-14 14:00:11 +0700

    update

--- resources/views/admin/adSpentMarketPlace/ads_cpas_index.blade.php
+++ resources/views/admin/adSpentMarketPlace/ads_cpas_index.blade.php
@@ -298,7 +298,7 @@
                         d.date_start = moment(dates[0], 'DD/MM/YYYY').format('YYYY-MM-DD');
                         d.date_end = moment(dates[1], 'DD/MM/YYYY').format('YYYY-MM-DD');
                     }
-                    d.kategori_produk = $('#kategoriProdukFilter').val();
+                    d.kategori_produk = filterCategory.val();
                 }
             },
             columns: [
