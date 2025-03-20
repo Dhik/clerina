032922a97ea6d378cf4162da5ecefd6c1eb767e8
Dhik <mohammad18018@mail.unpad.ac.id>
2025-03-20 14:51:10 +0700

    update

--- app/Domain/Sales/Models/NetProfit.php
+++ app/Domain/Sales/Models/NetProfit.php
@@ -2,13 +2,14 @@
 
 namespace App\Domain\Sales\Models;
 
-use App\Domain\Tenant\Models\Tenant;
+use App\Domain\Tenant\Traits\FilterByTenant;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class NetProfit extends Model
 {
+    use FilterByTenant;
     protected $table = 'net_profits';
     protected $fillable = [
         'date',
@@ -30,24 +31,4 @@ class NetProfit extends Model
     protected $casts = [
         'date' => 'date'
     ];
-    /**
-     * Retrieve the model for a bound value.
-     *
-     * @param  mixed  $value
-     * @param  string|null  $field
-     */
-    public function resolveRouteBinding($value, $field = null): ?Model
-    {
-        // Customize the query to retrieve the model based on your requirements
-        return $this->where('id', $value)
-            ->where('tenant_id', auth()->user()->current_tenant_id)
-            ->first();
-    }
-    /**
-     * relations to tenant
-     */
-    public function tenant(): BelongsTo
-    {
-        return $this->belongsTo(Tenant::class);
-    }
 }
