0e81a81705242ba92aefa6d53016b3b3dfafa1b8
Dhik <mohammad18018@mail.unpad.ac.id>
2025-03-25 12:00:25 +0700

    update

--- app/Domain/Sales/Controllers/NetProfitController.php
+++ app/Domain/Sales/Controllers/NetProfitController.php
@@ -242,7 +242,7 @@ class NetProfitController extends Controller
     {
         try {
             $startDate = now()->startOfMonth();
-            $tenant_id = Auth::user()->current_tenant_id;
+            $tenant_id = 2;
             
             $dates = collect();
             for($date = clone $startDate; $date->lte(now()); $date->addDay()) {
