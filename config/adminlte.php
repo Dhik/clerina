b0ec83a23da5f837308a2db641cb4952e198d685
Dhik <mohammad18018@mail.unpad.ac.id>
2025-04-11 13:57:00 +0700

    update

--- config/adminlte.php
+++ config/adminlte.php
@@ -338,9 +338,15 @@ return [
         //     'icon' => 'nav-icon far fa-circle text-info',
         //     'can' => [PermissionEnum::ViewMarketing],
         // ],
+        // [
+        //     'text' => 'Customer',
+        //     'url' => 'admin/cstmr_analysis',
+        //     'icon' => 'nav-icon far fa-circle text-info',
+        //     'can' => [PermissionEnum::ViewCustomer],
+        // ],
         [
             'text' => 'Customer',
-            'url' => 'admin/cstmr_analysis',
+            'url' => 'admin/customer',
             'icon' => 'nav-icon far fa-circle text-info',
             'can' => [PermissionEnum::ViewCustomer],
         ],
