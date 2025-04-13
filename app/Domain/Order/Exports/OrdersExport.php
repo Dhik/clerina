9d378b7513a760e0391e76ee71ece4c95fc3a697
Dhik <mohammad18018@mail.unpad.ac.id>
2025-04-13 07:36:58 +0700

    update

--- app/Domain/Order/Exports/OrdersExport.php
+++ app/Domain/Order/Exports/OrdersExport.php
@@ -44,7 +44,7 @@ class OrdersExport implements FromQuery, ShouldAutoSize, WithColumnFormatting, W
             ->where('tenant_id', $this->tenantId)
             ->where('date', '>=', $this->startDate)
             ->where('date', '<=', $this->endDate)
-            ->where('sales_channel_id', 1)
+            ->where('sales_channel_id', 4)
             ->orderBy('date', 'asc');
     }
 
