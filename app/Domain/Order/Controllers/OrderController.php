57ba844e1cf344a2f0abadb94eebb58c21f443be
Dhik <mohammad18018@mail.unpad.ac.id>
2025-04-15 11:48:16 +0700

    update

--- app/Domain/Order/Controllers/OrderController.php
+++ app/Domain/Order/Controllers/OrderController.php
@@ -3933,8 +3933,7 @@ class OrderController extends Controller
 
     public function updateSuccessDateFromSheet()
     {
-        set_time_limit(0);
-        $this->googleSheetService->setSpreadsheetId('1hMubpvYFyDnPJB3NtiOwH-nH0Qwb9wz7Sq4laVESvPM');
+        $this->googleSheetService->setSpreadsheetId('1RDC3Afs4wzaO3S36rvX35xB_D_zuqVs5vfMe7TI8vRY');
         $range = 'Shopee Balance!A:G';
         $sheetData = $this->googleSheetService->getSheetData($range);
 
