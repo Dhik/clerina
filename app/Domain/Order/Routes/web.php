f48e81520959a5ea445b1e9321c9078b1c536ff8
Dhik <mohammad18018@mail.unpad.ac.id>
2025-04-16 12:09:20 +0700

    update

--- app/Domain/Order/Routes/web.php
+++ app/Domain/Order/Routes/web.php
@@ -53,6 +53,7 @@ Route::prefix('admin')
                 Route::get('/import_cleora_crm2', [OrderController::class, 'importClosingIis'])->name('order.import_cleora_crm2');
                 Route::get('/import_cleora_crm3', [OrderController::class, 'importClosingKiki'])->name('order.import_cleora_crm3');
                 Route::get('/import_cleora_crm4', [OrderController::class, 'importClosingZalsa'])->name('order.import_cleora_crm4');
+                Route::get('/import_cleora_crm5', [OrderController::class, 'importClosingRani'])->name('order.import_cleora_crm5');
                 Route::get('/import_balance_shopee', [OrderController::class, 'updateSuccessDateShopee'])->name('order.import_balance_shopee');
                 Route::get('/import_balance_shopee2', [OrderController::class, 'updateSuccessDateShopee2'])->name('order.import_balance_shopee2');
                 Route::get('/import_balance_shopee3', [OrderController::class, 'updateSuccessDateShopee3'])->name('order.import_balance_shopee3');
