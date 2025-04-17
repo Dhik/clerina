7c10fb653056acf608d38102468cdb89b496e8c6
Dhik <mohammad18018@mail.unpad.ac.id>
2025-04-17 13:44:45 +0700

    update

--- database/migrations/2025_04_17_133747_create_laporan_keuangan_table.php
+++ database/migrations/2025_04_17_133747_create_laporan_keuangan_table.php
@@ -14,27 +14,28 @@ return new class extends Migration
         Schema::create('laporan_keuangan', function (Blueprint $table) {
             $table->id();
             $table->date('date')->nullable();
+            $table->unsignedBigInteger('tenant_id')->nullable();
             $table->decimal('sales', 15, 2)->nullable();
+            $table->decimal('b2b_sales', 15, 2)->nullable();
+            $table->decimal('crm_sales', 15, 2)->nullable();
             $table->decimal('marketing', 15, 2)->nullable();
             $table->decimal('spent_kol', 15, 2)->nullable();
             $table->decimal('affiliate', 15, 2)->nullable();
             $table->decimal('operasional', 15, 2)->nullable();
             $table->decimal('hpp', 15, 2)->nullable();
+            $table->decimal('fee_packing', 15, 2)->nullable();
             $table->decimal('roas', 30, 2)->nullable();
+            $table->decimal('romi', 30, 2)->nullable();
             $table->bigInteger('visit')->nullable();
             $table->bigInteger('qty')->nullable();
             $table->bigInteger('order')->nullable();
             $table->decimal('closing_rate', 30, 2)->nullable();
-            $table->decimal('fee_packing', 15, 2)->nullable()->after('hpp');
-            $table->decimal('romi', 30, 2)->nullable()->after('roas');
-            $table->unsignedBigInteger('tenant_id')->nullable()->after('date');
-            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('cascade');
-            $table->decimal('b2b_sales', 15, 2)->nullable()->after('sales');
-            $table->decimal('crm_sales', 15, 2)->nullable()->after('b2b_sales');
-            $table->decimal('balance_amount', 15, 2)->nullable()->after('closing_rate');
-            $table->decimal('gross_revenue', 15, 2)->nullable()->after('balance_amount');
-            $table->decimal('fee_admin', 15, 2)->nullable()->after('gross_revenue');
+            $table->decimal('balance_amount', 15, 2)->nullable();
+            $table->decimal('gross_revenue', 15, 2)->nullable();
+            $table->decimal('fee_admin', 15, 2)->nullable();
             $table->timestamps();
+            
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('cascade');
         });
     }
 
