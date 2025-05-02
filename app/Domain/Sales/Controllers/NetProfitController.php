961fa888352a49531b8ff32b0eaf0c32efc62d5c
Dhik <mohammad18018@mail.unpad.ac.id>
2025-05-02 18:15:59 +0700

    update

--- app/Domain/Sales/Controllers/NetProfitController.php
+++ app/Domain/Sales/Controllers/NetProfitController.php
@@ -1796,7 +1796,7 @@ class NetProfitController extends Controller
         }
         $currentTenantId = 1;
         
-        $now = Carbon::create(2025, 4, 1);
+        $now = Carbon::create(2025, 5, 1);
         $startDate = $now->copy()->startOfMonth()->format('Y-m-d');
         $endDate = $now->copy()->endOfMonth()->format('Y-m-d');
         
@@ -1895,7 +1895,7 @@ class NetProfitController extends Controller
         }
         
         // Update sheet name to April 2025
-        $sheetName = 'Daily Count April 2025';
+        $sheetName = 'SalesReport';
         
         $this->googleSheetService->clearRange("$sheetName!A1:Z1000");
         $this->googleSheetService->exportData("$sheetName!A1", $data, 'USER_ENTERED');
