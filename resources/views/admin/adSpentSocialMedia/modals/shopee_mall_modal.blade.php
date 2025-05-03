cc06d8cad4af3dc5f4403887fb84b76cbca42503
Dhik <mohammad18018@mail.unpad.ac.id>
2025-05-03 08:36:48 +0700

    update

--- resources/views/admin/adSpentSocialMedia/modals/shopee_mall_modal.blade.php
+++ resources/views/admin/adSpentSocialMedia/modals/shopee_mall_modal.blade.php
@@ -26,6 +26,7 @@
                             <option value="Glowsmooth">Glowsmooth</option>
                             <option value="Red Saviour">Red Saviour</option>
                             <option value="3 Minutes">3 Minutes</option>
+                            <option value="Calendula">Calendula</option>
                             <option value="Lain-lain">Lain-lain</option>
                         </select>
                     </div>
