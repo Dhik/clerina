af18ef97956f85e5807cc4002a9300e7211b0725
Dhik <mohammad18018@mail.unpad.ac.id>
2025-05-26 13:42:12 +0700

    update

--- app/Domain/Campaign/Requests/KeyOpinionLeaderRequest.php
+++ app/Domain/Campaign/Requests/KeyOpinionLeaderRequest.php
@@ -14,7 +14,7 @@ class KeyOpinionLeaderRequest extends FormRequest
         $isUpdate = $this->isMethod('PUT') || $this->isMethod('PATCH');
 
         return [
-            'channel' => [$isUpdate ? 'nullable' : 'required', Rule::in(KeyOpinionLeaderEnum::Channel)],
+            'channel' => $isUpdate ? ['nullable'] : ['required', Rule::in(KeyOpinionLeaderEnum::Channel)],
             'username' => ['required', 'regex:/^[a-zA-Z0-9_]+$/', Rule::unique('key_opinion_leaders')->where(function ($query) {
                 // For updates, get the current channel; for creates, use the input channel
                 if ($this->isMethod('PUT') || $this->isMethod('PATCH')) {
@@ -23,13 +23,13 @@ class KeyOpinionLeaderRequest extends FormRequest
                 }
                 return $query->where('channel', $this->channel);
             })->ignore($kolId)],
-            'niche' => [$isUpdate ? 'nullable' : 'required', Rule::in(KeyOpinionLeaderEnum::Niche)],
-            'average_view' => [$isUpdate ? 'nullable' : 'required', 'numeric', 'integer'],
-            'skin_type' => [$isUpdate ? 'nullable' : 'required', Rule::in(KeyOpinionLeaderEnum::SkinType)],
-            'skin_concern' => [$isUpdate ? 'nullable' : 'required', Rule::in(KeyOpinionLeaderEnum::SkinConcern)],
-            'content_type' => [$isUpdate ? 'nullable' : 'required', Rule::in(KeyOpinionLeaderEnum::ContentType)],
-            'rate' => [$isUpdate ? 'nullable' : 'required', 'numeric', 'integer'],
-            'pic_contact' => [$isUpdate ? 'nullable' : 'required', 'exists:users,id'],
+            'niche' => $isUpdate ? ['nullable'] : ['required', Rule::in(KeyOpinionLeaderEnum::Niche)],
+            'average_view' => $isUpdate ? ['nullable'] : ['required', 'numeric', 'integer'],
+            'skin_type' => $isUpdate ? ['nullable'] : ['required', Rule::in(KeyOpinionLeaderEnum::SkinType)],
+            'skin_concern' => $isUpdate ? ['nullable'] : ['required', Rule::in(KeyOpinionLeaderEnum::SkinConcern)],
+            'content_type' => $isUpdate ? ['nullable'] : ['required', Rule::in(KeyOpinionLeaderEnum::ContentType)],
+            'rate' => $isUpdate ? ['nullable'] : ['required', 'numeric', 'integer'],
+            'pic_contact' => $isUpdate ? ['nullable'] : ['required', 'exists:users,id'],
             'phone_number' => ['nullable', 'string'],
             'views_last_9_post' => ['nullable', 'boolean'],
             'activity_posting' => ['nullable', 'boolean'],
