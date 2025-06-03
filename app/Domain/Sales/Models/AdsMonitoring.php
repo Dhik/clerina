18fd54f0f86e9f360ad987e5e7972c589215ad02
Dhik <mohammad18018@mail.unpad.ac.id>
2025-06-03 10:50:28 +0700

    update

--- app/Domain/Sales/Models/AdsMonitoring.php
+++ app/Domain/Sales/Models/AdsMonitoring.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class AdsMeta3 extends Model
+class AdsMonitoring extends Model
 {
     use HasFactory;
 
