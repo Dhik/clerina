ee57505b29800d2367f54bca7a9d2efbdf87b61b
Dhik <mohammad18018@mail.unpad.ac.id>
2025-06-10 13:58:17 +0700

    update

--- app/Domain/ContentPlan/Routes/web.php
+++ app/Domain/ContentPlan/Routes/web.php
@@ -15,29 +15,27 @@ Route::prefix('admin')
         Route::prefix('contentPlan')
             ->group(function () {
 
-                // Standard CRUD routes
+                // PUT THESE API ROUTES FIRST (before the /{contentPlan} routes)
+                Route::get('/data', [ContentPlanController::class, 'data'])->name('contentPlan.data');
+                Route::get('/status-counts', [ContentPlanController::class, 'getStatusCounts'])->name('contentPlan.statusCounts');
+                Route::post('/store', [ContentPlanController::class, 'store'])->name('contentPlan.store');
+
+                // Standard CRUD routes (THESE COME AFTER)
                 Route::get('/', [ContentPlanController::class, 'index'])->name('contentPlan.index');
                 Route::get('/create', [ContentPlanController::class, 'create'])->name('contentPlan.create');
                 Route::get('/{contentPlan}', [ContentPlanController::class, 'show'])->name('contentPlan.show');
                 Route::get('/{contentPlan}/edit', [ContentPlanController::class, 'edit'])->name('contentPlan.edit');
-
-                // API Routes for data processing
-                Route::get('/data', [ContentPlanController::class, 'data'])->name('contentPlan.data');
-                Route::post('/store', [ContentPlanController::class, 'store'])->name('contentPlan.store');
                 Route::put('/{contentPlan}/update', [ContentPlanController::class, 'update'])->name('contentPlan.update');
                 Route::delete('/{contentPlan}/destroy', [ContentPlanController::class, 'destroy'])->name('contentPlan.destroy');
+                Route::get('/{contentPlan}/details', [ContentPlanController::class, 'getDetails'])->name('contentPlan.details');
+
+                // Step-specific routes
+                Route::get('/{contentPlan}/step/{step}', [ContentPlanController::class, 'editStep'])
+                    ->name('contentPlan.editStep')
+                    ->where('step', '[1-6]');
                 
                 Route::put('/{contentPlan}/step/{step}/update', [ContentPlanController::class, 'updateStep'])
                     ->name('contentPlan.updateStep')
-                    ->where('step', '[1-6]');
-                
-                // Additional API endpoints
-                Route::get('/{contentPlan}/details', [ContentPlanController::class, 'getDetails'])->name('contentPlan.details');
-                Route::get('/status-counts', [ContentPlanController::class, 'getStatusCounts'])->name('contentPlan.statusCounts');
-                
-                // Step-specific routes for workflow
-                Route::get('/{contentPlan}/step/{step}', [ContentPlanController::class, 'editStep'])
-                    ->name('contentPlan.editStep')
                     ->where('step', '[1-6]');
             });
     });
