86b302d41bddf59a24310fe58f1669d58e042774
Dhik <mohammad18018@mail.unpad.ac.id>
2025-06-19 10:10:48 +0700

    update

--- resources/views/admin/affiliate_shopee/index.blade.php
+++ resources/views/admin/affiliate_shopee/index.blade.php
@@ -302,9 +302,5 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.1/dist/sweetalert2.all.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-
-<script>
-// Paste the complete JavaScript code from the previous artifact here
 @include('admin.affiliate_shopee.scripts')
-</script>
 @stop
